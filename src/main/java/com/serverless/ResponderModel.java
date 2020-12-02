76a6f0e7d2c8e6802d4140d529aca56e19183889
sebas <sbitashenaol@gmail.com>
2020-12-02 20:51:58 +0000

    ajuste return

--- src/main/java/com/serverless/ResponderModel.java
+++ src/main/java/com/serverless/ResponderModel.java
@@ -7,35 +7,38 @@ public class ResponderModel {
     private final String teamName = "dca";
 
     public String answer(String question) {
+        String respuesta = "";
         if ("".equals(question)){
-            return teamName;
+            respuesta = teamName;
         }
 
         Matcher resp1 = Pattern.compile(".*what is your name").matcher(question);
         if (resp1.matches()) {
-            return teamName;
+            respuesta =  teamName;
         }
 
         Matcher resp2 = Pattern.compile(".*what is (\\d+) plus (\\d+)").matcher(question);
         if (resp2.matches()) {
-            return String.valueOf(Integer.parseInt(resp2.group(1)) + Integer.parseInt(resp2.group(2)));
+            respuesta =  String.valueOf(Integer.parseInt(resp2.group(1)) + Integer.parseInt(resp2.group(2)));
         }
 
         Matcher resp3 = Pattern.compile(".*which of the following numbers is the largest: (\\d+), (\\d+)").matcher(question);
         if (resp3.matches()) {
             if (Integer.parseInt(resp3.group(1)) > Integer.parseInt(resp3.group(2))) {
-                return String.valueOf(Integer.parseInt(resp3.group(1)));
+                respuesta = String.valueOf(Integer.parseInt(resp3.group(1)));
             } else {
-                return String.valueOf(Integer.parseInt(resp3.group(2)));
+                respuesta = String.valueOf(Integer.parseInt(resp3.group(2)));
             }
         }
 
+        Matcher resp4 = Pattern.compile(".*which of the following numbers is the largest: (\\d+), (\\d+)").matcher(question);
+
+        //which of the following numbers is the largest: 892, 97, 70, 508
+
         Matcher sumMatcher = Pattern.compile(".*what is the sum of (\\d+) and (\\d+)").matcher(question);
         if (sumMatcher.matches()) {
-            return String.valueOf(Integer.parseInt(sumMatcher.group(1)) + Integer.parseInt(sumMatcher.group(2)));
+            respuesta =  String.valueOf(Integer.parseInt(sumMatcher.group(1)) + Integer.parseInt(sumMatcher.group(2)));
         }
-        
-        return teamName;
+        return respuesta;
     }
-
 }
